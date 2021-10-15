1da63caeca660014074d7fdae090016704c5bc3b
Gutu Ivan <gutsu.ivan@mail.ru>
2021-10-15 08:45:48 +0300

    Initial commit

---
+++ src/main/java/dev/gutuivan/Pw2TaskGalerieApplication.java
@@ -0,0 +1,13 @@
+package dev.gutuivan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pw2TaskGalerieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pw2TaskGalerieApplication.class, args);
+    }
+
+}
